19f89f99b767bb6b4c4b0f43c4d6697f47554bf3
RICARDO JAIRI SILVESTRE <jairircrd@gmail.com>
2026-08-31 20:21:26 -0300

    Add FilmeDocumentario class with details display

--- Aula05/FilmeDocumentario.java
+++ Aula05/FilmeDocumentario.java
@@ -1,1 +1,14 @@
+public class FilmeDocumentario extends Filme {
+    private String tema;
 
+    public FilmeDocumentario(String titulo, int duracao, String classificacao, String tema) {
+        super(titulo, duracao, classificacao);
+        this.tema = tema;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Tema: " + tema);
+    }
+}
