ebe45e1963ac194fa8523b9cd3e16b07bd99845a
RICARDO JAIRI SILVESTRE <jairircrd@gmail.com>
2026-08-31 20:29:13 -0300

    Add Funcionario class with basic salary calculation

---
+++ Aula05/Exercicio02/Funcionario.java
@@ -0,0 +1,21 @@
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+
+    public Funcionario(String nome, double salarioBase) {
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+    }
+
+    public double calcularPagamento() {
+        return salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+}
